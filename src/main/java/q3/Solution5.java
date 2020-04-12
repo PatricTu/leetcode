f74171bb45f043d1951b1012bb705568a394d24c
Patrick <patrictxc@gmail.com>
2020-04-12 22:11:36 +0800

    solved q3 with sliding window

--- src/main/java/q3/Solution5.java
+++ src/main/java/q3/Solution5.java
@@ -1,19 +1,31 @@
 package q3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * sliding window
  * time complexity: O(n)
  */
 public class Solution5 {
     public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0)
-            return 0;
         int maxLength = 0;
-        // TODO
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            if (map.containsKey(c)) {
+                left = Math.max(left, map.get(c) + 1);
+            }
+            map.put(c, right);
+            right++;
+            maxLength = Math.max(maxLength, right - left);
+        }
         return maxLength;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution5().lengthOfLongestSubstring(" "));
+        System.out.println(new Solution5().lengthOfLongestSubstring("234563789"));
     }
 }
